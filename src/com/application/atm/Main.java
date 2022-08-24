6d3e05a6dc66f1cc3a3298628594000e5890cb18
ishan <abadheshmishra01@gmail.com>
2022-08-24 13:27:07 +0530

    Hello

--- src/com/application/atm/Main.java
+++ src/com/application/atm/Main.java
@@ -3,7 +3,8 @@ package com.application.atm;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Hello, World! :)");
 
     }
 }
